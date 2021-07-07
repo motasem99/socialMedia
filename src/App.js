bb5bc43cba6173728d480d4349d0cd53a422436c
mutasem <motasemimad10@gmail.com>
2021-07-07 12:13:07 +0300

    setup nav and style it

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import './App.css';
 import 'antd/dist/antd.css';
 
+import NavBar from './components/NavBar/NavBar.jsx';
+
 const App = () => {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <div>
+        <NavBar />
+      </div>
+    </div>
+  );
 };
 
 export default App;
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,80 @@
+import React, { useState } from 'react';
+import { Button } from 'antd';
+import { BellOutlined, HomeOutlined, PlusOutlined } from '@ant-design/icons';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  width: 100%;
+  height: 70px;
+  background-color: #00bcd4;
+  color: white;
+  box-shadow: 0px 2px 4px -1px rgb(0 0 0 / 20%),
+    0px 4px 5px 0px rgb(0 0 0 / 14%), 0px 1px 10px 0px rgb(0 0 0 / 12%);
+`;
+
+const ContainerUserExists = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+`;
+
+const PlusOutlinedIcon = styled(PlusOutlined)`
+  font-size: 1.5rem;
+  margin-left: 1rem;
+  margin-right: 1rem;
+  cursor: pointer;
+  color: white;
+`;
+
+const HomeOutlinedIcon = styled(HomeOutlined)`
+  font-size: 1.5rem;
+  margin-left: 1rem;
+  margin-right: 1rem;
+  cursor: pointer;
+  color: white;
+`;
+
+const BellOutlinedIcon = styled(BellOutlined)`
+  font-size: 1.5rem;
+  margin-left: 1rem;
+  margin-right: 1rem;
+  cursor: pointer;
+  color: white;
+`;
+
+const LoginButton = styled(Button)`
+  color: white;
+  font-size: 1rem;
+  font-weight: 500;
+`;
+
+const SignupButton = styled(Button)`
+  color: white;
+  font-size: 1rem;
+  font-weight: 500;
+`;
+
+const NavBar = () => {
+  const [userExists, setUserExists] = useState(false);
+
+  return (
+    <Container>
+      {userExists ? (
+        <ContainerUserExists>
+          <PlusOutlinedIcon />
+          <HomeOutlinedIcon />
+          <BellOutlinedIcon />
+        </ContainerUserExists>
+      ) : (
+        <ContainerUserExists>
+          <LoginButton type='text'>Login</LoginButton>
+          <HomeOutlinedIcon />
+          <SignupButton type='text'>Signup</SignupButton>
+        </ContainerUserExists>
+      )}
+    </Container>
+  );
+};
+
+export default NavBar;
