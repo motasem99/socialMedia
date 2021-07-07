3283f3a8a78f91a7d7cb2950b62e26455d089fc7
mutasem <motasemimad10@gmail.com>
2021-07-07 13:35:08 +0300

    start with sign up page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Button } from 'antd';
 
 const Home = () => {
   return (
     <div>
       <h1>Home page</h1>
+      <Button type='primary'>Primary Button</Button>
     </div>
   );
 };
--- src/pages/Signup/Signup.jsx
+++ src/pages/Signup/Signup.jsx
@@ -1,10 +1,35 @@
 import React from 'react';
 
+import logo from '../../image/logoSocialMedia.png';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  margin-top: 2rem;
+`;
+
+const InfoContent = styled.div`
+  width: 50%;
+  margin: 0 auto;
+`;
+
+const Img = styled.img`
+  width: 70px;
+  height: 70px;
+`;
+
+const H2 = styled.h2`
+  font-size: 4rem;
+  font-weight: 200;
+`;
+
 const Signup = () => {
   return (
-    <div>
-      <h1>Sign up page</h1>
-    </div>
+    <Container>
+      <InfoContent>
+        <Img src={logo} alt='logo' />
+        <H2>Sign up</H2>
+      </InfoContent>
+    </Container>
   );
 };
 
