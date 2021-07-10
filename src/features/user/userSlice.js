7c16acd9730d4dbdfffa78f54629337c50429abd
mutasem <motasemimad10@gmail.com>
2021-07-10 20:19:32 +0300

    clear

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -19,7 +19,6 @@ export const { setName } = counterSlice.actions;
 export const selectUser = (state) => state.user.user;
 
 export const signup = (data) => async (dispatch, getState) => {
-  console.log(data);
   try {
     await axios
       .post(`${process.env.REACT_APP_SOCIAL_MEDIA_URL}/api/users/signup`, {
@@ -29,7 +28,6 @@ export const signup = (data) => async (dispatch, getState) => {
         handle: data.userHandle,
       })
       .then((res) => {
-        console.log(res.data.token);
         localStorage.setItem('token', res.data.token);
       })
       .catch((err) => {
