89930bbe28ab3e20f62133ce366c8fd3f0ed44ef
mutasem <motasemimad10@gmail.com>
2021-07-10 20:19:11 +0300

    handle signup users and get token and sve in local storage

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -1,4 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
 
 export const counterSlice = createSlice({
   name: 'user',
@@ -17,8 +18,26 @@ export const { setName } = counterSlice.actions;
 
 export const selectUser = (state) => state.user.user;
 
-export const signup = (data) => (dispatch, getState) => {
+export const signup = (data) => async (dispatch, getState) => {
   console.log(data);
+  try {
+    await axios
+      .post(`${process.env.REACT_APP_SOCIAL_MEDIA_URL}/api/users/signup`, {
+        email: data.email,
+        password: data.password,
+        confirmPassword: data.confirmPassword,
+        handle: data.userHandle,
+      })
+      .then((res) => {
+        console.log(res.data.token);
+        localStorage.setItem('token', res.data.token);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  } catch (err) {
+    throw err;
+  }
 };
 
 export default counterSlice.reducer;
